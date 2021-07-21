6f2f6f481462a562432a5e9bfe6a56705c94762f
Hamsa-GRL <hamsa@graniteriverlabs.in>
2021-07-21 23:35:11 +0530

    Site under construction

--- src/App.js
+++ src/App.js
@@ -5,22 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is my sample portfolio          
-        </a>
-        <button> Click me </button>
-        <button className="bg-red-500 hover:bg-blue-700 text-white font-bold m-4 py-2 px-4 rounded-full">
-            Tailwind Button
-        </button>
+      Hey There! I'm under construction.
       </header>
     </div>
   );
